bace2004ac03cbe14c4b61c89c0ac9d0a1253c9a
Edward_MIT <usmanoff19980404@gmail.com>
2025-03-03 01:09:42 +0900

    feat: built project starndarts

---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,20 @@
+export enum Message {
+  SOMETHING_WENT_WRONG = "Something went wrong!",
+  NO_DATA_FOUND = "No data is found!",
+  CREATE_FAILED = "Create is failed!",
+  UPDATE_FAILED = "Update is failed!",
+  REMOVE_FAILED = "Remove failed!",
+  UPLOAD_FAILED = "Upload failed!",
+  BAD_REQUEST = "Bad request!",
+
+  NO_MEMBER_NICK = "No member with that nickname!",
+  BLOCKED_USER = "You have been blocked, contact restaurant! ",
+  WRONG_PASSWORD = "Password is incorrect, please try again!",
+  NOT_AUTHENTICATED = "You are not authenticated, Please login first!",
+  TOKEN_NOT_EXIST = "Bearer Token is not provided!",
+  ONLY_SPECIFIC_ROLES_ALLOWED = "Allowed only for members with specific role! ",
+  NOT_ALLOWED_REQUEST = "Not allowed request!",
+  PROVIDE_ALLOWED_FORMAT = "Please provide jpg, jpeg, or png images!",
+  SELF_SUBSCRIPTION_DENIED = "Self subscription denied!",
+  USED_NICK_PHONE = "You are inserting already used nick or phone number!",
+}
