da88249a5726931d494ff05811af0e64ee3d99f3
Edward_MIT <usmanoff19980404@gmail.com>
2025-03-02 21:40:15 +0900

    fix: installation packages & modify format standarts

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -4,8 +4,7 @@ import { AppService } from './app.service';
 import {ConfigModule} from "@nestjs/config"
 
 @Module({
-  imports: [
-    ConfigModule.forRoot()
+  imports: [ConfigModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService],
