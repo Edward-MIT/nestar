4b5715770bbd79c742ede681c31a76ceebb6c8b0
Edward_MIT <usmanoff19980404@gmail.com>
2025-03-07 00:03:09 +0900

    feat: MIT task ZK was done

--- train.ts
+++ train.ts
@@ -1,15 +1,30 @@
+// TASK ZK
+function printNumbers(): void {
+    let num: number = 1;
+    const interval: NodeJS.Timeout = setInterval(() => {
+      console.log(num);
+      if (num === 5) {
+        clearInterval(interval); // 5 ga yetganda intervalni to‘xtatamiz
+      }
+      num++;
+    }, 1000);
+  }
+
+  printNumbers();
+
+
 // MIT TASK ZJ
 
-function reduceNestedArray(arr: any[]): number {
-    return arr.reduce((sum, item) => {
-        if (Array.isArray(item)) {
-            return sum + reduceNestedArray(item);
-        }
-        if (typeof item === 'number') {
-            return sum + item;
-        }
-        return sum;
-    }, 0);
-}
+// function reduceNestedArray(arr: any[]): number {
+//     return arr.reduce((sum, item) => {
+//         if (Array.isArray(item)) {
+//             return sum + reduceNestedArray(item);
+//         }
+//         if (typeof item === 'number') {
+//             return sum + item;
+//         }
+//         return sum;
+//     }, 0);
+// }
 
-console.log(reduceNestedArray([1, [1, 2, [4]]]));
+// console.log(reduceNestedArray([1, [1, 2, [4]]]));
