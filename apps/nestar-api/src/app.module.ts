e47cf9416f93d2297742f702f4a2ef7cbfa3745b
Edward_MIT <usmanoff19980404@gmail.com>
2025-03-14 23:47:12 +0900

    fix: modified

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -1,35 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {ConfigModule} from "@nestjs/config";
-import {GraphQLModule} from "@nestjs/graphql";
-import {ApolloDriver} from "@nestjs/apollo"
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
-import { AuthModule } from './componenets/auth/auth.module';
 import { T } from './libs/types/common';
-
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     GraphQLModule.forRoot({
-       driver:ApolloDriver,
-       playground: true,
-       uploads: false,
-       autoSchemaFile:true,
-       formatError:(error: T) => {
-        const graphQLFormattedError = {
-          code: error?.extensions?.exception?.response?.message || error?.extensions?.response?.message || error?. message,
+      driver: ApolloDriver,
+      playground: true,
+      uploads: false,
+      autoSchemaFile: true,
+      formatError: (error: T) => {
+        const graphqlFormattedError = {
+          code: error?.extensions.code,
+          message: error?.extensions?.response?.message || error?.extensions?.response?.message || error?.message,
         };
-        console.log("GRAPHQL GLOBAL ERR:", graphQLFormattedError );
-        return graphQLFormattedError;
-       },
-  }),
-      ComponentsModule,
-      DatabaseModule,
-      AuthModule],
+
+        console.log('GraphQL global Error:', graphqlFormattedError);
+        return graphqlFormattedError;
+      },
+    }),
+    ComponentsModule,  //HTTP
+    DatabaseModule,   //TCP
+    //Webscket
+    //SocketId
+  ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
 })
