1c9a50aec62e3e1ace7e6c95d4e2991096623e08
Edward_MIT <usmanoff19980404@gmail.com>
2025-03-18 11:04:12 +0900

    fix: creation and verification jwt

--- apps/nestar-api/src/components/auth/auth.service.ts
+++ apps/nestar-api/src/components/auth/auth.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcyrpt from 'bcryptjs'
+import { Member } from '../../libs/dto/member/member';
+import { T } from '../../libs/types/common';
 
 @Injectable()
 export class AuthService {
+  constructor(private jwtServise: JwtService) {}
+
+
   public async hashPassword(memberPassword: string): Promise<string> {
     const salt = await bcyrpt.genSalt();
     return await bcyrpt.hash(memberPassword, salt);
@@ -10,5 +16,21 @@ export class AuthService {
 
   public async comparePasswords(password: string, hashedPassword: string): Promise<boolean> {
    return await bcyrpt.compare(password, hashedPassword);
+  }
+
+  public async createToken(member: Member): Promise<string> {
+    const payload: T = {};
+
+    Object.keys(member['_doc'] ? member ["_doc"] : member).map((ele) => {
+      payload[`${ele}`] = member [`${ele}`];
+    });
+    delete payload.memberPassword;
+
+    return await this.jwtServise.signAsync(payload);
+  }
+
+  public async verifyToken(token: string): Promise<Member>{
+    const member = await this.jwtServise.verifyAsync(token);
+    return member;
   }
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -16,11 +16,10 @@ export class MemberService {
  ) {}
 
  public async signup (input: MemberInput): Promise<Member>{
-  // TO DO Hash password
   input.memberPassword = await this.authService.hashPassword(input.memberPassword);
   try{
     const result = await this.memberModel.create(input);
-    // TODO : Authentication via Token
+   result.accessToken = await this.authService.createToken(result);
     return result
 
   }catch(err) {
@@ -43,10 +42,9 @@ export class MemberService {
   }else if(response.memberStatus === MemberStatus.BLOCK) {
     throw new InternalServerErrorException(Message.BLOCKED_USER);
   }
-  // TODO compare passwords
     const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword)
-    if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD)
-
+    if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+    response.accessToken = await this.authService.createToken(response);
 
   return response;
  }
