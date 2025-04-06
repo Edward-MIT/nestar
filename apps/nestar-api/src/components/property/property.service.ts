3cbf22e2fbf4602bde7f581a5eea014748178063
Edward_MIT <usmanoff19980404@gmail.com>
2025-04-06 17:24:40 +0900

    feat: integration of like related complex lookup query

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -11,7 +11,7 @@ import { StatisticModifier, T } from '../../libs/types/common';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import * as moment from 'moment';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
-import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
+import { lookupAuthMemberLiked, lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
 import { LikeService } from '../like/like.service';
 import { LikeInput } from '../../libs/dto/like/like.input';
 import { LikeGroup } from '../../libs/enums/like.enum';
@@ -116,7 +116,7 @@ export class PropertyService {
                       { $skip: (input.page - 1) * input.limit },
                       { $limit: input.limit },
 
-                      // meLiked
+                      lookupAuthMemberLiked(memberId),
 
                       lookupMember,
                       { $unwind: '$memberData' },
